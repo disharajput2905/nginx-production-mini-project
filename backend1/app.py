3e929848590c4d9afa74cbef9ec247c2521a39ea
Disha Rajpoot <disharajput2905@gmail.com>
2026-05-19 16:22:20 +0000

    added ssl certificate and static site page

--- backend1/app.py
+++ backend1/app.py
@@ -1,11 +1,9 @@
 from flask import Flask
-import socket
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Hello from Backend 1 | host:{socket.gethostname()}\n"
+    return f"Backend 1 - port 5000\n"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(port=5000)
--- backend2/app.py
+++ backend2/app.py
@@ -1,11 +1,9 @@
 from flask import Flask
-import socket
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Hello from Backend 2 | host:{socket.gethostname()}\n"
+    return f"backend 2 - port5001\n"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(port=5001)
